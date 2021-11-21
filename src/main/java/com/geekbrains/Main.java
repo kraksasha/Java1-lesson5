2ac3601723aa1d4ce0f1ce1171ba391e51424ee4
kraksasha <kraksasha@yandex.ru>
2021-11-21 16:05:34 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/Main.java
@@ -0,0 +1,24 @@
+package com.geekbrains;
+
+public class Main {
+    public static void main(String[] args) {
+        Person persArray[] = new Person[5];
+        persArray[0] = new Person("Петров Иван Сергеевич", "Java-daveloper","1111@yandex.ru",
+                "89254362815",150000,35);
+        persArray[1] = new Person("Иванов Сергей Анатольевич", "Java-daveloper","2222@yandex.ru",
+                "89224364835",140000,45);
+        persArray[2] = new Person("Андреев Антон Васильевич", "Java-daveloper","1212@yandex.ru",
+                "89254467868",160000,37);
+        persArray[3] = new Person("Мухин Евгений Иванович", "Java-daveloper","1313@yandex.ru",
+                "89268249014",120000,34);
+        persArray[4] = new Person("Песков Артем Владимирович", "Java-daveloper","1414@yandex.ru",
+                "89285652868",135000,42);
+
+        for (int i = 0; i < persArray.length; i++){
+            if (persArray[i].getAge() > 40){
+                persArray[i].infoAboutPerson();
+            }
+        }
+
+    }
+}
---
+++ src/main/java/com/geekbrains/Person.java
@@ -0,0 +1,73 @@
+package com.geekbrains;
+
+public class Person {
+    private String fio;
+    private String position;
+    private String email;
+    private String phonenumber;
+    private int salary;
+    private int age;
+
+    public Person(String fio, String position, String email, String phonenumber, int salary, int age){
+        this.fio = fio;
+        this.position = position;
+        this.email = email;
+        this.phonenumber = phonenumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void infoAboutPerson(){
+        System.out.println("ФИО: " + fio + " Должность: " + position +
+                " Email: " + email + " Номер телефона: " + phonenumber +
+                " Зарплата: " + salary + " Возраст: " + age);
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
